17397a7403289cff23918aa89bf57416aff2215d
Pavel Vashkel <p.vashkel@datarockets.com>
2020-11-15 16:14:27 +0300

    Add implementation of EpoxyModelTouchCallback to handle dnd movements

---
+++ app/src/main/java/com/vshkl/epoxyhandledraganddropexample/MainEpoxyTouchCallback.kt
@@ -0,0 +1,49 @@
+package com.vshkl.epoxyhandledraganddropexample
+
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.epoxy.EpoxyModelTouchCallback
+import com.airbnb.epoxy.EpoxyViewHolder
+import com.vshkl.epoxyhandledraganddropexample.epoxymodels.RowEpoxyModel
+import com.vshkl.epoxyhandledraganddropexample.epoxymodels.RowEpoxyModel_
+
+class MainEpoxyTouchCallback(
+    controller: MainController,
+    private val listener: OnRowMoveListener
+) : EpoxyModelTouchCallback<RowEpoxyModel>(controller, RowEpoxyModel::class.java) {
+
+    interface OnRowMoveListener {
+
+        fun onMoved(movingRowId: String, shiftingRowId: String)
+
+    }
+
+    override fun onMoved(
+        recyclerView: RecyclerView?,
+        viewHolder: EpoxyViewHolder?,
+        fromPos: Int,
+        target: EpoxyViewHolder?,
+        toPos: Int,
+        x: Int,
+        y: Int
+    ) {
+        val movingItem = viewHolder?.model
+            ?.takeIf { it is RowEpoxyModel_ }
+            ?.let { (it as RowEpoxyModel_).rowId }
+        val shiftingItem = target?.model
+            ?.takeIf { it is RowEpoxyModel_ }
+            ?.let { (it as RowEpoxyModel_).rowId }
+
+        if (movingItem != null && shiftingItem != null) {
+            listener.onMoved(movingItem, shiftingItem)
+        }
+
+        super.onMoved(recyclerView, viewHolder, fromPos, target, toPos, x, y)
+    }
+
+    override fun getMovementFlagsForModel(model: RowEpoxyModel?, adapterPosition: Int) =
+        makeMovementFlags(ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0)
+
+    override fun isLongPressDragEnabled() = false
+
+}
